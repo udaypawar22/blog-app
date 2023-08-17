c08e759e5e0dc6909156eaacc257c7109c752386
Uday Pawar <udaysinghpawar0302@gmail.com>
2023-08-17 09:49:51 +0530

    bottom padding

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 export default function Layout() {
   return (
-    <div className="">
+    <div className="pb-12">
       <Header />
       <Outlet />
     </div>
